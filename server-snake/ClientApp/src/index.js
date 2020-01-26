e6833e2d28fc1f582ac7997c73fa8a140ad5eda9
Tao Gómez Gil <tgomezgil@talentsoft.com>
2020-01-26 17:11:41 +0100

    Remove useless files and code

--- server-snake/ClientApp/src/index.js
+++ server-snake/ClientApp/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Board from './Board';
 import { game } from "./game";
 import { createStore } from "redux";
